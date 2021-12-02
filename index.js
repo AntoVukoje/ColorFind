7c532110a0b9fd7e80f4fe2a2a6c4360bb9ee450
Anto Vukoje <72924054+AntoVukoje@users.noreply.github.com>
2021-12-02 12:02:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 buttons = document.querySelectorAll("button");
 paragraphs = document.querySelectorAll("p");
-buttons_len = buttons.length;
+btn_len = buttons.length;
 
-for(let i=0; i < buttons_len; i++){
+for(let i=0; i < btn_len; i++){
     buttons[i].addEventListener("click", function(){
         navigator.clipboard.writeText(paragraphs[i].innerText);
-        alert("Color " + paragraphs[i].innerText + " has been added to clipboard!");
+        alert("Copied " + paragraphs[i].innerText);
     });
 }
